759913aedb4844b4f8154a6e35cc08ef051ab69c
kylealexxander <pyeliteman113@gmail.com>
2023-08-08 11:07:56 -0700

    php backend

--- windows_app/core/functions.php
+++ windows_app/core/functions.php
@@ -1,13 +1,5 @@
 <?php
-// +------------------------------------------------------------------------+
-// | @author Deen Doughouz (DoughouzForest)
-// | @author_url 1: http://www.wowonder.com
-// | @author_url 2: http://codecanyon.net/user/doughouzforest
-// | @author_email: wowondersocial@gmail.com   
-// +------------------------------------------------------------------------+
-// | WoWonder - The Ultimate Social Networking Platform
-// | Copyright (c) 2016 WoWonder. All rights reserved.
-// +------------------------------------------------------------------------+
+
 $non_allowed = array(
     'password',
     'background_image',
